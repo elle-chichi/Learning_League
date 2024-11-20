5c9243fc5c6ae9ac8b49bc0e8abf943cf769ad9b
elle-chichi <mchellechi@gmail.com>
2024-11-20 21:22:42 +0300

    forth commit

--- theLearningLeague/settings.py
+++ theLearningLeague/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'theLearningLeague.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learningleague',
+        'USER': 'root',
+        'PASSWORD': 'Elle2024!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
